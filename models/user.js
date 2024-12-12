7492d8de42cc9f3dd946d0c5ca0888ed0b054cec
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-13 00:01:54 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,17 +1,53 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {}
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.ENUM
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+
+  User.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Name is required' },
+          notEmpty: { msg: 'Name cannot be empty' },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: { msg: 'Email is required' },
+          isEmail: { msg: 'Invalid email format' },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: { msg: 'Password is required' },
+          notEmpty: { msg: 'Password cannot be empty' },
+        },
+      },
+      role: {
+        type: DataTypes.ENUM('ADMIN', 'STAFF', 'User'),
+        defaultValue: 'User',
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+
   return User;
 };
+
