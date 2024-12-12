7126854eeaacd3fd4a4476803413a8d3163060ab
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-13 00:04:29 +0200

    Update 20241212200629-create-user.js

--- migrations/20241212200629-create-user.js
+++ migrations/20241212200629-create-user.js
@@ -7,38 +7,37 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: DataTypes.INTEGER,
       },
       name: {
-        type: Sequelize.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            notNull: true || 'Name is required'
-        }
-    },
-    email: {
-        type: Sequelize.STRING,
+          notNull: { msg: 'Name is required' },
+          notEmpty: { msg: 'Name cannot be empty' },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
         allowNull: false,
         unique: true,
         validate: {
-            notNull: true || 'Email is required',
-            isEmail: true || 'Invalid email format',
-            isUnique: true || 'Email already exists'
-        }
-    },
-    password: {
-        type: Sequelize.STRING,
+          notNull: { msg: 'Email is required' },
+          isEmail: { msg: 'Invalid email format' },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
         allowNull: false,
         validate: {
-            notNull: true || 'Password is required'
-        }
-    },
-    role: {
-        type: Sequelize.ENUM,
-        allowNull: false,
-        values: ['ADMIN', 'STAFF', 'User'],
-        defaultValue: 'User'
-    },
+          notNull: { msg: 'Password is required' },
+          notEmpty: { msg: 'Password cannot be empty' },
+        },
+      },
+      role: {
+        type: DataTypes.ENUM('ADMIN', 'STAFF', 'User'),
+        defaultValue: 'User',
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
