90c8bc6f605ae36212804987a83d5ab931b9c7a4
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-12 21:37:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const { PORT } = process.env;
-
+const  sequelize = require('./config/connectDB');
 
 
 
 const app = express();
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+sequelize.sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+      console.log('Database connected successfully');
+    });
+  })
+  .catch((err) => {
+    console.error('Unable to sync database:', err);
+  });
