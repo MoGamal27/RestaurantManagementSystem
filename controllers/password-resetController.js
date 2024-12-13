0296d470e506bc5b672ac3a7d021c20702c55667
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-13 20:44:30 +0200

    feat: add paswword reset controller

---
+++ controllers/password-resetController.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+const { Token, User }  = require('../models');
+const asyncHandler = require('express-async-handler');
+const appError = require('../utils/appError');
+const crypto = require('crypto');
+const sendEmail = require('../Services/emailService');
+const bcrypt = require('bcrypt');
+
+
+// send password reset link
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+
+    const user = await User.findOne({ where:{ email: req.body.email} });
+
+    if (!user) {
+        return next(new appError("User not found", 404));
+    }
+    
+    
+
+    let token = await Token.findOne({ where: { user_id: user.id  } });
+        if (!token) {
+            token = await new Token({
+                user_id: user.id,
+                token: crypto.randomBytes(32).toString("hex"),
+            }).save();
+        }
+
+        const link = `${process.env.BASE_URL}/${user.id}/${token.token}`;
+        await sendEmail({
+            email: user.email,
+            subject: "Reset Password",
+            html: `<a href="${link}">Reset Password</a>`,
+        });
+
+        res.send("password reset link sent to your email account");
+
+
+})
+
+// Reset user password
+exports.resetUserPassword = asyncHandler(async (req, res, next) => {
+
+    const user = await User.findByPk(req.params.userId);
+
+   if (!user) {
+        return next(new appError("User not found", 404));
+    }
+    
+
+    const token = await Token.findOne({
+        where: {
+            user_id: user.id,
+            token: req.params.token,
+        }
+    });
+
+    if (!token) return res.status(400).json({
+        success: false,
+        status: 'fail',
+        message: 'Invalid token'
+    });
+      
+       const { password } = req.body;
+       
+       if(!password) return res.status(400).json({
+        success: false,
+        status: 'fail',
+        message: 'Please enter your password'
+    });
+
+       const hashedPassword = await bcrypt.hash(password, 10);
+        user.password = hashedPassword;
+
+        await user.save();
+        await token.destroy();
+
+        res.send("password reset sucessfully.");
+
+})
