f1c19a29fad386ac2a36402f673b0b89ece6ea7d
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-13 08:14:00 +0200

    add category in main routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
+const categoryRoutes = require('./categoryRoutes');
 
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
+router.use('/categories', categoryRoutes);
 
 module.exports = router;
