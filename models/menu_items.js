6b6161a92eda3e1760fd66f0891c3ceae01c85d6
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-13 09:08:21 +0200

    feat: add menu items model

---
+++ models/menu_items.js
@@ -0,0 +1,55 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Menu_items extends Model {
+    static associate(models) {
+      Menu_items.belongsTo(models.Category, {
+        foreignKey: 'category_id'
+      });
+    }
+  }
+  Menu_items.init({
+    name: { 
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: {
+        msg: 'A menu item name is required'
+      }
+    }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A menu item description is required'
+        }
+      }
+    },
+    price: {
+       type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A menu item price is required'
+        }
+      }
+    },
+    category_id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     validate: {
+     notNull: {
+       msg: 'A menu item category is required'
+     }
+     }
+    }
+  }, {
+    sequelize,
+    modelName: 'Menu_items',
+  });
+  return Menu_items;
+};
