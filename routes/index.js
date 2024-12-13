c2e2fd29c344e679fe8b74ab9909110e0b1b5406
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-13 18:00:59 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ const authRoutes = require('./authRoutes');
 const userRoutes = require('./userRoutes');
 const categoryRoutes = require('./categoryRoutes');
 const menu_itemsRoutes = require('./menu_itemsRoutes');
+const orderRoutes = require('./orderRoutes');
+const order_itemsRoutes = require('./order_itemsRoutes');
 
 
 router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/menu_items', menu_itemsRoutes);
+router.use('/orders', orderRoutes);
+router.use('/order_items', order_itemsRoutes);
 
 module.exports = router;
