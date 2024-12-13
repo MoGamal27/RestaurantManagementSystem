f76928f4e97cd5a7b9c5fb905d0e205356629145
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-13 17:59:57 +0200

    feat: add order endpoints

---
+++ routes/orderRoutes.js
@@ -0,0 +1,21 @@
+const { createOrder, getAllOrders, getOrder, updateOrder, deleteOrder, completeOrder } = require('../controllers/orderController');
+const verifyToken = require('../middleware/verifyToken');
+const verifyRole = require('../middleware/verifyRole');
+const userRoles = require('../utils/userRoles');
+const { orderValidator } = require('../utils/validators/orderValidator');
+const router = require('express').Router();
+
+
+router.post("/", orderValidator, verifyToken, verifyRole(userRoles.ADMIN), createOrder);
+
+router.get("/", verifyToken, verifyRole(userRoles.ADMIN), getAllOrders);
+
+router.get("/:orderId", verifyToken, verifyRole(userRoles.ADMIN), getOrder);
+
+router.put("/:orderId", verifyToken, verifyRole(userRoles.ADMIN), updateOrder);
+
+router.delete("/:orderId", verifyToken, verifyRole(userRoles.ADMIN), deleteOrder);
+
+router.put("/:orderId/complete", verifyToken, verifyRole(userRoles.ADMIN), completeOrder);
+
+module.exports = router
