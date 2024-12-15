cf401df276dc3d9d30c829e11c63fab86a518240
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-16 00:02:03 +0200

    Update order_itemsValidator.js

--- utils/validators/order_itemsValidator.js
+++ utils/validators/order_itemsValidator.js
@@ -2,6 +2,12 @@ const { check } = require('express-validator');
 const validatorMiddleware = require('../../middleware/validatorMiddleware');
 
 exports.order_itemsValidator = [
+    check('order_id')
+    .notEmpty()
+    .withMessage('Order ID is required')
+    .isNumeric()
+    .withMessage('Order ID must be a number'),
+
     check('quantity')
     .notEmpty()
     .withMessage('Quantity is required')
@@ -14,8 +20,19 @@ exports.order_itemsValidator = [
         return true;
     }),
 
+    check('unit_price')
+    .notEmpty()
+    .withMessage('Unit price is required')
+    .isNumeric()
+    .withMessage('Unit price must be a number'),
 
-    check('menuItemId')
+    check('subtotal')
+    .notEmpty()
+    .withMessage('Subtotal is required')
+    .isNumeric()
+    .withMessage('Subtotal must be a number'),
+
+    check('menu_item_id')
     .notEmpty()
     .withMessage('Menu item ID is required'),
 
