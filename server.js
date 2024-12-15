e5ed7b7258ca292820f2318b8fc4739d41a04d65
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-15 13:26:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,30 @@ const express = require('express');
 const { PORT } = process.env;
 const db = require('./models');
 const routes = require('./routes/index');
+const appError = require('./utils/appError');
+const globalError = require('./middleware/errorMiddleware');
 const initializeCronJobs = require('./config/cronJobs')
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./utils/swagger')
+
+
 const app = express();
 
 app.use(express.json());
 
+
+
+
+// main system routes
 app.use('/api', routes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.all("*", (req, res, next) => {
+  next(new appError(`Can't find this route ${req.originalUrl}`, 400));
+});
+
+// Global Error Handling Middleware For Express
+app.use(globalError);
 
 db.sequelize.sync({alter: true})
   .then(() => {
