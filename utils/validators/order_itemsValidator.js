c9152ca93dc80863c32710d7bbb5fcfaa526d0de
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-15 13:26:30 +0200

    Update order_itemsValidator.js

--- utils/validators/order_itemsValidator.js
+++ utils/validators/order_itemsValidator.js
@@ -2,16 +2,39 @@ const { check } = require('express-validator');
 const validatorMiddleware = require('../../middleware/validatorMiddleware');
 
 exports.order_itemsValidator = [
-    check('user_id')
+    check('order_id')
     .notEmpty()
-    .withMessage('User is required')
+    .withMessage('Order ID is required')
     .isNumeric()
-    .withMessage('User must be a number'),
+    .withMessage('Order ID must be a number'),
 
     check('quantity')
     .notEmpty()
     .withMessage('Quantity is required')
     .isNumeric()
-    .withMessage('Quantity must be a number'),
+    .withMessage('Quantity must be a number')
+    .custom((value, { req }) => {
+        if (value <= 0) {
+            throw new Error('Quantity must be greater than 0');
+        }
+        return true;
+    }),
+
+    check('unit_price')
+    .notEmpty()
+    .withMessage('Unit price is required')
+    .isNumeric()
+    .withMessage('Unit price must be a number'),
+
+    check('subtotal')
+    .notEmpty()
+    .withMessage('Subtotal is required')
+    .isNumeric()
+    .withMessage('Subtotal must be a number'),
+
+    check('menu_item_id')
+    .notEmpty()
+    .withMessage('Menu item ID is required'),
+
     validatorMiddleware
 ]
