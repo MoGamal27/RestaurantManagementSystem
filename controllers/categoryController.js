212ba5d6e32bc2990f02dd04076e0012694eba0f
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-15 21:53:40 +0200

    update category controller: add comments

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,12 +1,41 @@
 const { Category } = require('../models');
 const HandlerFactory = require('./handlerFactory');
 
+/**
+ * @route POST api/categories
+ * @desc Create a category
+ * @access Admin only
+ * */
 exports.createCategory = HandlerFactory.createOne(Category)
 
+
+/**
+ * @route GET api/categories
+ * @desc Get all categories
+ * @access Public
+ * */
 exports.getAllCategories = HandlerFactory.getAll(Category)
 
+
+/**
+ * @route GET api/categories/:id
+ * @desc Get a single category
+ * @access Public
+ */
 exports.getCategory = HandlerFactory.getOne(Category)
 
+
+/**
+ * @route PUT api/categories/:id
+ * @desc Update a category
+ * @access Admin only
+ */ 
 exports.updateCategory = HandlerFactory.updateOne(Category)
 
+
+/**
+ * @route DELETE api/categories/:id
+ * @desc Delete a category
+ * @access Admin only
+ */
 exports.deleteCategory = HandlerFactory.deleteOne(Category)
