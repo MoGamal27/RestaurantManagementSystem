3aa671238f52be3e88caf7b9415220151aca3a5c
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-15 13:19:50 +0200

    fix issue hashpassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,10 +8,12 @@ exports.signup = asyncHandler(async (req, res, next) => {
    
         const { name, email, password, role } = req.body;
 
+        const hashedPassword = await bcrypt.hash(password, 10);
+
         const user = await User.create({
             name,
             email,
-            password,
+            password: hashedPassword,
             role
         }); 
 
