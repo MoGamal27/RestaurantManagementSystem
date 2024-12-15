fbd8a78f915ef7a9cf75b5fc51fb724327490095
Mohamed Gamal <113600854+MoGamal27@users.noreply.github.com>
2024-12-15 21:51:04 +0200

    Update auth: add comments

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,14 @@ const appError = require('../utils/appError');
 const bcrypt = require('bcrypt');
 const asyncHandler = require('express-async-handler');
 
+
+/**
+ * @desc    Signup a new user
+ * @route   POST /api/auth/signup
+ * @access  Public
+ * @returns {Object} - User object
+ * @throws {Error} - If user already exists
+ */
 exports.signup = asyncHandler(async (req, res, next) => {
    
         const { name, email, password, role } = req.body;
@@ -28,6 +36,14 @@ exports.signup = asyncHandler(async (req, res, next) => {
     
     });
     
+
+ /**
+   * @desc    Signin a user
+     * @route   POST /api/auth/signin
+     * @access  Public
+     * @returns {Object} - User object
+     * @throws {Error} - If user does not exist or password is incorrect
+     */
 
 exports.signIn = asyncHandler(async (req, res, next) => {
     
